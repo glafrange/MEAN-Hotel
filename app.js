7f4c0ec14bb0a153cd361e9f3d275483ed7559bb
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-21 12:35:56 -0400

    video 3 - require and module.exports

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var name = "Everyone!";
-console.log("Hello " + name);
+require('./instantHello');
+var goodbye = require('./talk/goodbye');
+var talk = require('./talk');
+var question = require('./talk/question')
+
+talk.intro();
+talk.hello("Gabe");
+
+var answer = question.ask("What is the meaning of life?");
+console.log(answer);
+
+goodbye();
