50c63198524134140c430117419a2266bdcf8dae
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-22 12:36:03 -0400

    Video 21 - querying the database from node.js

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,13 +1,11 @@
 var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function(req, res){
 
   var db = dbconn.get();
-
-  console.log("db", db);
-
-  console.log("GET the hotels");
+  var collection = db.collection('hotels');
 
   var offset = 0;
   var count = 5;
@@ -19,21 +17,34 @@ module.exports.hotelsGetAll = function(req, res){
     count = parseInt(req.query.count, 10);
   }
 
-  var returnData = hotelData.slice(offset, offset+count);
+  collection
+    .find()
+    .skip(offset)
+    .limit(count)
+    .toArray(function(err, docs) {
+      console.log("Found hotels", docs);
+      res
+        .status(200)
+        .json(docs);
+    });
 
-  console.log(req.query);
-  res
-    .status(200)
-    .json(returnData);
 };
 
 module.exports.hotelsGetOne = function(req, res){
+  var db = dbconn.get();
+  var collection = db.collection('hotels');
+
   var hotelId = req.params.hotelId;
   console.log("GET hotelId", hotelId);
-  var thisHotel = hotelData[hotelId];
-  res
-    .status(200)
-    .json(thisHotel);
+  collection
+    .findOne({
+      _id : ObjectId(hotelId)
+    }, function(err, doc) {
+      res
+      .status(200)
+      .json(doc);
+    });
+
 };
 
 module.exports.hotelsAddOne = function(req, res) {
